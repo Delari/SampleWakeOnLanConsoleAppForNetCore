9fb3fa1c1653cda1ede1ff844be9e0f4fcbdd412
Delarians <alexsifm@gmail.com>
2021-12-24 10:12:03 +0500

    Добавьте файлы проекта.

---
+++ SampleWakeOnLanConsoleAppForNetCore/Program.cs
@@ -0,0 +1,33 @@
+﻿
+//New format on NET version 6
+
+var macAddress = "00:30:67:D5:8D:56"; // Test device MAC address
+await SampleWakeOnLanConsoleAppForNetCore.WakeOnLanClass.WakeOnLan(macAddress);
+Console.WriteLine("Package Sending!");
+Console.WriteLine("Press any key for close!");
+Console.ReadKey();
+Console.WriteLine("Closing");
+
+
+
+// OLD FORMAT on NET version 5
+//namespace SampleWakeOnLanConsoleAppForNetCore
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            MainAsync().GetAwaiter().GetResult();
+//            Console.WriteLine("Package Sending!");
+//            Console.WriteLine("Press any key for close!");
+//            Console.ReadKey();
+//            Console.WriteLine("Closing");
+//        }
+
+//        private static async Task MainAsync()
+//        {
+//            var macAddress = "00:30:67:D5:8D:56";
+//            await WakeOnLanClass.WakeOnLan(macAddress);
+//        }
+//    }
+//}
---
+++ SampleWakeOnLanConsoleAppForNetCore/WakeOnLanClass.cs
@@ -0,0 +1,81 @@
+﻿using System.Net.NetworkInformation;
+using System.Net;
+using System.Net.Sockets;
+using System.Text.RegularExpressions;
+
+namespace SampleWakeOnLanConsoleAppForNetCore
+{
+    public static class WakeOnLanClass
+    {
+        public static async Task WakeOnLan(string macAddress)
+        {
+            byte[] magicPacket = BuildMagicPacket(macAddress);
+            foreach (NetworkInterface networkInterface in NetworkInterface.GetAllNetworkInterfaces().Where((n) =>
+                n.NetworkInterfaceType != NetworkInterfaceType.Loopback && n.OperationalStatus == OperationalStatus.Up))
+            {
+                IPInterfaceProperties iPInterfaceProperties = networkInterface.GetIPProperties();
+                foreach (MulticastIPAddressInformation multicastIPAddressInformation in iPInterfaceProperties.MulticastAddresses)
+                {
+                    IPAddress multicastIpAddress = multicastIPAddressInformation.Address;
+                    if (multicastIpAddress.ToString().StartsWith("ff02::1%", StringComparison.OrdinalIgnoreCase)) // Ipv6: All hosts on LAN (with zone index)
+                    {
+                        UnicastIPAddressInformation unicastIPAddressInformation = iPInterfaceProperties.UnicastAddresses.Where((u) =>
+                            u.Address.AddressFamily == AddressFamily.InterNetworkV6 && !u.Address.IsIPv6LinkLocal).FirstOrDefault();
+                        if (unicastIPAddressInformation != null)
+                        {
+                            await SendWakeOnLan(unicastIPAddressInformation.Address, multicastIpAddress, magicPacket);
+                            break;
+                        }
+                    }
+                    else if (multicastIpAddress.ToString().Equals("224.0.0.1")) // Ipv4: All hosts on LAN
+                    {
+                        UnicastIPAddressInformation unicastIPAddressInformation = iPInterfaceProperties.UnicastAddresses.Where((u) =>
+                            u.Address.AddressFamily == AddressFamily.InterNetwork && !iPInterfaceProperties.GetIPv4Properties().IsAutomaticPrivateAddressingActive).FirstOrDefault();
+                        if (unicastIPAddressInformation != null)
+                        {
+                            await SendWakeOnLan(unicastIPAddressInformation.Address, multicastIpAddress, magicPacket);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        // MacAddress in any standard HEX format
+        static byte[] BuildMagicPacket(string macAddress) 
+        {
+            macAddress = Regex.Replace(macAddress, "[: -]", "");
+            byte[] macBytes = new byte[6];
+            for (int i = 0; i < 6; i++)
+            {
+                macBytes[i] = Convert.ToByte(macAddress.Substring(i * 2, 2), 16);
+            }
+
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (BinaryWriter bw = new BinaryWriter(ms))
+                {
+                    //First 6 times 0xff
+                    for (int i = 0; i < 6; i++)  
+                    {
+                        bw.Write((byte)0xff);
+                    }
+                    // then 16 times MacAddress
+                    for (int i = 0; i < 16; i++) 
+                    {
+                        bw.Write(macBytes);
+                    }
+                }
+                // 102 bytes magic packet
+                return ms.ToArray(); 
+            }
+        }
+        static async Task SendWakeOnLan(IPAddress localIpAddress, IPAddress multicastIpAddress, byte[] magicPacket)
+        {
+            using (UdpClient client = new UdpClient(new IPEndPoint(localIpAddress, 0)))
+            {
+                await client.SendAsync(magicPacket, magicPacket.Length, multicastIpAddress.ToString(), 9);
+            }
+        }
+    }
+}
